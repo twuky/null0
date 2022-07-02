69028d2a5eaa0b6780f0a75c023e39ce507dd20d
David Konsumer <konsumer@jetboystudio.com>
2022-07-02 15:33:21 -0700

    making progress with c runtime

--- example/index.ts
+++ example/index.ts
@@ -1,37 +1,11 @@
 // assemblyscript example
 
-import {
-  log,
-  cls,
-  drawText,
-  getFPS,
-  loadFont,
-  loadImage,
-  loadMusic,
-  palette,
-  playMusic,
-  setTitle,
-  imageDimensions,
-  drawImage,
-  drawSprite
-} from "../target/assemblyscript/null0"
+// log a string
+@external("env", "null0_log")
+declare function log(message: string): void
+
 
 export function init(): void  {
   log("init")
 }
 
-export function update(delta:u16): void {
-  // log('update: ' + delta.toString())
-}
-
-export function loaded(resource:u16):void {
-  // log('loaded: ' + resource.toString())
-}
-
-export function buttonDown(button:u8): void {
-  // log('buttonDown: ' + button.toString())
-}
-
-export function buttonUp(button:u8): void {
-  // log('buttonUp: ' + button.toString())
-}
--- runtime/native/src/main.c
+++ runtime/native/src/main.c
@@ -5,63 +5,119 @@
 #include "wasm3.h"
 #include "m3_env.h"
 
-#define FATAL(func, msg) { fprintf(stderr, "Fatal: %s - %s\n", func, msg); return 1; }
+static M3Environment* env;
+static M3Runtime* runtime;
+static M3Module* module;
 
-m3ApiRawFunction(null0_log) {
-  m3ApiGetArgMem(char*, text)
-  printf("%s", text);
-  m3ApiSuccess();
+// throw a fatal error
+static void null0_fatal_error(char* func, char* msg) {
+  fprintf(stderr, "FATAL: %s - %s\n", func, msg);
+  exit(1);
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage %s game.wasm\n", argv[0]);
-    return 1;
+// all wasm3 functions return same sort of error-pattern, so this wraps that
+static void null0_check_wasm3 (M3Result result) {
+  if (result) {
+    M3ErrorInfo info;
+    m3_GetErrorInfo(runtime, &info);
+    char *message;
+    sprintf(message, "%s - %s", result, info.message);
+    null0_fatal_error("wasm", message);
   }
+}
 
-  IM3Environment env = m3_NewEnvironment ();
-  IM3Runtime runtime = m3_NewRuntime (env, 1024, NULL);
-  IM3Module module;
+// this checks the general state of the runtime, to make sure there are no errors lingering
+static void null0_check_wasm3_is_ok () {
+  M3ErrorInfo error;
+  m3_GetErrorInfo(runtime, &error);
+  if (error.result) {
+    char *message;
+    sprintf(message, "%s - %s", error.result, error.message);
+    null0_fatal_error("wasm", message);
+  }
+}
+
+// exports from cart
+static M3Function* cart_init;
+
+
+// imports for cart
+// XXX: currently only pruints first character
+static m3ApiRawFunction (null0_log) {
+  m3ApiGetArgMem(const char*, str);
+  printf("%s", str);
+  m3ApiSuccess();
+}
+static m3ApiRawFunction (null0_abort) {
+  m3ApiGetArgMem(const char*, message);
+  m3ApiGetArgMem(const char*, fileName);
+  m3ApiGetArg(int, lineNumber);
+  m3ApiGetArg(int, columnNumber);
+  char *out;
+  sprintf(out, "%s in %s:%d:%d", message, fileName, lineNumber, columnNumber);
+  null0_fatal_error("cart", out);
+}
+
+// this is the actual run, after all has been loaded
+void null0_cart_run () {
+  if (cart_init) {
+    null0_check_wasm3(m3_CallV(cart_init));
+  } else {
+    null0_fatal_error("cart", "no init!");
+  }
+}
+
+// load a wasm binary buffer
+void null0_load_cart_wasm (u8* wasmBuffer, int byteLength) {
+  env = m3_NewEnvironment();
+  runtime = m3_NewRuntime (env, 1024, NULL);
+  null0_check_wasm3(m3_ParseModule (env, &module, wasmBuffer, byteLength));
+  null0_check_wasm3(m3_LoadModule(runtime, module));
+
+  // imports for cart
+  m3_LinkRawFunction(module, "env", "null0_log", "v(i)", &null0_log);
+  m3_LinkRawFunction(module, "env", "abort", "v(iiii)", &null0_abort);
+  null0_check_wasm3_is_ok();
+
+  // exports from cart
+  m3_FindFunction(&cart_init, runtime, "init");
+}
+
+// load a binary wasm file
+void null0_load_cart_file (char* filename) {
   u8* wasm = NULL;
-  u32 fsize = 0;
-  M3Result result = m3Err_none;
+  int fsize = 0;
   
-  FILE* f = fopen (argv[1], "rb");
-  if (!f) FATAL("wasm", "open");
+  FILE* f = fopen (filename, "rb");
+  if (!f) null0_fatal_error("file", "open - Cannot open file.");
   fseek (f, 0, SEEK_END);
   fsize = ftell(f);
   fseek (f, 0, SEEK_SET);
 
   if (fsize < 8) {
-    FATAL("wasm", "file is too small");
+    null0_fatal_error("file", "size - File is too small.");
   } else if (fsize > 64*1024*1024) {
-    FATAL("wasm", "file is too big");
+    null0_fatal_error("file", "size - File is too big.");
   }
 
   wasm = (u8*) malloc(fsize);
   if (!wasm) {
-    FATAL("wasm", "cannot allocate memory for wasm binary");
+    null0_fatal_error("file", "memory - Cannot allocate memory for wasm binary.");
   }
 
   if (fread (wasm, 1, fsize, f) != fsize) {
-    FATAL("wasm", "cannot read file");
+    null0_fatal_error("file", "read - Cannot read file.");
   }
   fclose (f);
   f = NULL;
 
-  result = m3_ParseModule (env, &module, wasm, fsize);
-  if (result) FATAL("wasm", "cannot parse");
-  result = m3_LoadModule (runtime, module);
-  if (result) FATAL("wasm", "cannot load");
-
-  m3_LinkRawFunction (module, "env", "null0_log", "v(i)", &null0_log);
-
-  IM3Function gameInit;
-  result = m3_FindFunction (&gameInit, runtime, "init");
-  if ( result ) {
-    FATAL("init", result);
-  }
-  if (gameInit) {
-    m3_CallV (gameInit);
-  }
+  null0_load_cart_wasm(wasm, fsize);
+  null0_cart_run();
 }
+
+
+int main (int argc, char **argv) {
+  null0_load_cart_file(argv[1]);
+  return 0;
+}
+
